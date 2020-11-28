6dfca383526fc7deffb0646eeacb3cff837a5b53
Alexander Sekunov <ansekunov@yandex.ru>
2020-11-28 14:51:05 +0300

    Добавьте файлы проекта.

---
+++ Amaranth/View/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Amaranth.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
